2bbdda4e77c67cf1b3fd1f14726c7fd8af42f0ff
Jason <k.jason0105@gmail.com>
2021-10-11 15:49:25 -0700

    create, delete, update, and fetch plant routes all done

--- routes/api/plants.js
+++ routes/api/plants.js
@@ -6,6 +6,16 @@ const Plant = require('../../models/Plant');
 const validatePlantInput = require('../../validation/plants');
 
 
+router.get('/user/:user_id', (req, res) => {
+  passport.authenticate('jwt', { session: false }),
+  Plant.find({user: req.params.user_id})
+      .then(plants => res.json(plants))
+      .catch(err =>
+          res.status(404).json({ noPlantFound: 'No plants found from this user' }
+      )
+  );
+});
+
 router.post('/create',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
