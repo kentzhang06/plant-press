90f6c9b25aea3539a7d93e0cf9c78360dead75e6
jayzizzle <dj.jonzamora@gmail.com>
2021-10-12 16:47:42 -0700

    begin styling homepage

---
+++ frontend/src/components/home_page/footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { SiHomeassistant } from 'react-icons/si';
+import { RiPlantLine, RiPlantFill, RiInformationLine, RiInformationFill } from 'react-icons/ri';
+import { FaRegBell, FaBell } from 'react-icons/fa';
+import { HiOutlinePhotograph, HiPhotograph } from 'react-icons/hi';
+
+export const Footer = (props) => {
+
+  const location = useLocation();
+
+  return (
+    <footer className='container-fluid'>
+      <div className='row'>
+        <div className='col d-flex justify-content-center align-items-center icon'>
+          <NavLink to='/feed'>
+            { location.pathname === '/feed' ? <HiPhotograph /> : <HiOutlinePhotograph /> }
+          </NavLink>
+        </div>
+        <div className='col d-flex justify-content-center align-items-center icon'>
+          <NavLink to='/'>
+            { location.pathname === '/collection' ? <RiPlantFill /> : <RiPlantLine /> }
+          </NavLink>
+        </div>
+        <div className='col d-flex justify-content-center align-items-center icon home my-auto'>
+          <NavLink to='/' className={ location.pathname === '/' ? 'indigo' : null }>
+            <SiHomeassistant />
+          </NavLink>
+        </div>
+        <div className='col d-flex justify-content-center align-items-center icon'>
+          <NavLink to='/'>
+            { location.pathname === '/reminders' ? <FaBell /> : <FaRegBell /> }
+          </NavLink>
+        </div>
+        <div className='col d-flex justify-content-center align-items-center icon'>
+          <NavLink to='/'>
+            { location.pathname === '/info' ? <RiInformationFill /> : <RiInformationLine /> }
+          </NavLink>
+        </div>
+      </div>
+    </footer>
+  )
+}
--- frontend/src/components/home_page/home_page.js
+++ frontend/src/components/home_page/home_page.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React from 'react';import { Footer } from './footer';
+
+import { FaRegBell, FaBell } from 'react-icons/fa';
+import { ImCheckboxUnchecked, ImCheckboxChecked } from 'react-icons/im';
 
 class HomePage extends React.Component {
 
-    render() {
-        return (
-            <div>
-                <h1>P P</h1>
-                <button onClick={this.props.logout}>Log out</button>
-                <footer>
-                    Copyright &copy; 2019 Chirper
-                </footer>
+  render() {
+
+    return(
+        <>
+          <div className='container-fluid'>
+
+            <div className='row d-flex justify-content-center break'>
+              <h4><FaBell className='break-icon'/>&nbsp;Reminders</h4>
             </div>
-        );
-    }
+
+            <div className='row d-flex justify-content-center reminder-row'>
+              <div className='col'>
+                Water Poison Ivy.<br />
+                <span className='note'>( 2 cups )</span>
+              </div>
+              <div className='col d-flex justify-content-end align-items-center'>
+                <ImCheckboxUnchecked />
+              </div>
+            </div>
+
+            <div className='row d-flex justify-content-center reminder-row done'>
+              <div className='col'>
+                Water Fern.<br />
+                <span className='note'>( 1 cup )</span>
+              </div>
+              <div className='col d-flex justify-content-end align-items-center'>
+                <ImCheckboxChecked />
+              </div>
+            </div>
+
+          </div>
+          <Footer />
+        </>
+    )
+  }
+
 }
 
 export default HomePage;
