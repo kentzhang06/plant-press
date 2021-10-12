13eaaa51684f4966646c326d94440687ee02eba0
Kent Zhang <kentzhang31@yahoo.com>
2021-10-11 17:25:02 -0700

    image upload setup

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,10 +6,13 @@ import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import HomePageContainer from './home_page/home_page_container';
 
+import PhotoForm from './photo/photo_form';
+
 const App = () => (
     <div>
 
         <Switch>
+            <Route exact path="/photo" component={PhotoForm} />
             <ProtectedRoute exact path="/pp" component={HomePageContainer} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
