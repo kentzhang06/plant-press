0880569b52c0ba011bfb2ff0155750afd0a16f3c
Kent Zhang <kentzhang31@yahoo.com>
2021-10-13 12:05:42 -0700

    saving current work

--- frontend/src/reducers/entities/entities_reducer.js
+++ frontend/src/reducers/entities/entities_reducer.js
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux";
-import PlantsReducer from "./plants_reducer";
 import PostsReducer from "./posts_reducer";
 import PlantsReducer from "./plants_reducer"
 import RemindersReducer from "./reminders_reducer";
