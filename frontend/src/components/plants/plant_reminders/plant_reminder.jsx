239bddcdcfc721656b208023ef2bda2e914ed140
Jason <k.jason0105@gmail.com>
2021-10-13 11:38:28 -0700

    plant reminder container

---
+++ frontend/src/components/plants/plant_reminders/plant_reminder.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class PlantReminder extends React.Component {
+  render () {
+    return null;
+  }
+}
+
+export default PlantReminder;
---
+++ frontend/src/components/plants/plant_reminders/plant_reminder_container.js
@@ -0,0 +1,2 @@
+import { connect } from 'react-redux';
+import PlantReminder from './plant_reminder';
