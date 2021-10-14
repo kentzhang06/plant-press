b1865eebd11befbd337111452f1bdf28df1cf738
jayzizzle <dj.jonzamora@gmail.com>
2021-10-14 09:33:51 -0700

    add add-a-plant link to my collection view

--- frontend/src/components/plants/plant_collection.js
+++ frontend/src/components/plants/plant_collection.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { RiPlantFill } from 'react-icons/ri';
+import { GoPlus } from 'react-icons/go';
 
 class PlantCollection extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
   componentDidMount() {
     this.props.fetchPlants(this.props.userId);
   }
@@ -17,6 +15,9 @@ class PlantCollection extends React.Component {
 
     return (
       <div className='container-fluid'>
+        <div className='row d-flex justify-content-center vertical-center heading'>
+          <h4><RiPlantFill className='heading-icon'/>&nbsp;My Collection</h4>
+        </div>
         {this.props.userPlants.map((plant, i) => (
           <Link to={`/plant/${plant._id}`}>
             <div className='row plant-row' key={plant._id}>
@@ -24,11 +25,29 @@ class PlantCollection extends React.Component {
                 {plant.name.slice(0,1)}
               </div>
               <div className='col-8 plant-row-text'>
-                {plant.name}
+                <div plant-row-info>
+                  <p className='plant-row-name'>{plant.name}</p>
+                  <p className='plant-row-type'>{plant.type}</p>
+                </div>
               </div>
-          </div>
+            </div>
           </Link>
         ))}
+        <Link to='/addplant'>
+            <div className='row plant-row-light'>
+              <div className='col-4 plant-row-img'>
+                <GoPlus />
+              </div>
+              <div className='col-8 plant-row-text'>
+                <div plant-row-info>
+                  <p className='plant-row-name'>
+                    Add A Plant
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Link>
+          <div className='row row-end'></div>
       </div>
     )
   }
