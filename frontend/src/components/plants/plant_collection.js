0090f0b99e89f398fe7696b8515592b2c9dfce81
jayzizzle <dj.jonzamora@gmail.com>
2021-10-13 17:58:57 -0700

    add partial styling to collection view

--- frontend/src/components/plants/plant_collection.js
+++ frontend/src/components/plants/plant_collection.js
@@ -17,21 +17,32 @@ class PlantCollection extends React.Component {
 
     return (
       <div className='container-fluid'>
-          <ul className='plants-row'>
-            {this.props.userPlants.map(plant =>
-
-              <li className='plant-box' key={plant._id}>
-                <Link to={`/plant/${plant._id}`}>
-                  <div className='plant-img'></div>
-                  {plant.name}
-                </Link>
-              </li>
-
-            )}
-          </ul>
-        </div>
+        {this.props.userPlants.map((plant, i) => (
+          <Link to={`/plant/${plant._id}`}>
+            <div className='row plant-row' key={plant._id}>
+              <div className='col-4 plant-row-img'>
+                {plant.name.slice(0,1)}
+              </div>
+              <div className='col-8 plant-row-text'>
+                {plant.name}
+              </div>
+          </div>
+          </Link>
+        ))}
+      </div>
     )
   }
 }
 
 export default PlantCollection;
+
+{/* <ul className='plants-row'>
+  {this.props.userPlants.map(plant =>
+    <li className='plant-box' key={plant._id}>
+      <Link to={`/plant/${plant._id}`}>
+        <div className='plant-img'></div>
+        {plant.name}
+      </Link>
+    </li>
+  )}
+</ul> */}
