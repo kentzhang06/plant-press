ee19b69510bae7a0fae0d884d7e7092741c95bc2
Jason <k.jason0105@gmail.com>
2021-10-14 17:38:10 -0700

    fixed bug where couldn't edit reminder text

--- validation/reminders.js
+++ validation/reminders.js
@@ -4,15 +4,10 @@ const validText = require('./valid-text');
 module.exports = function validateReminderInput(data) {
   let errors = {};
 
-  data.reminderType = validText(data.reminderType) ? data.reminderType : '';
   data.reminderText = validText(data.reminderText) ? data.reminderText : '';
 
   if (Validator.isEmpty(data.reminderText)) {
     errors.text = 'Text for reminder is required';
-  }
-
-  if (!Validator.isNumeric(data.frequency)) {
-    errors.text = "Must input reminder frequency";
   }
 
   return {
