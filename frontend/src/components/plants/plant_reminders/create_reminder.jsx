393cf539577f5fa28f598e32d60274c614a426ff
jayzizzle <dj.jonzamora@gmail.com>
2021-10-14 18:04:51 -0700

    save before merge to main

--- frontend/src/components/plants/plant_reminders/create_reminder.jsx
+++ frontend/src/components/plants/plant_reminders/create_reminder.jsx
@@ -56,8 +56,8 @@ class CreateReminder extends React.Component {
           <div className='d-flex justify-content-center form-padding'>
             <input type="number"
               value={this.state.frequency}
-              onChange={this.update('1')}
-              placeholder="#"
+              onChange={this.update('frequency')}
+              placeholder="0"
               min="1"
               max="100"
               className='select-num'
