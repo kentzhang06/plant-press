abc4b0fb2f69fff36515c35a5c8e6602b8f731e9
jayzizzle <dj.jonzamora@gmail.com>
2021-10-17 17:49:09 -0700

    re-style about us page

--- frontend/src/components/team_page/team_page.jsx
+++ frontend/src/components/team_page/team_page.jsx
@@ -1,103 +1,72 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ImLeaf } from 'react-icons/im';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const TeamPage = () => {
-  return (
+  const team = [
+    {
+      dev: 'Danny Feng',
+      github: 'https://github.com/dannyfeng1',
+      linkedIn: 'https://www.linkedin.com/in/danny-feng1/',
+    },
+    {
+      dev: 'Kent Zhang',
+      github: 'https://github.com/kentzhang06',
+      linkedIn: 'https://www.linkedin.com/in/kent-zhang-297591193/',
+    },
+    {
+      dev: 'Jason Kim',
+      github: 'https://github.com/jasonkim0105',
+      linkedIn: 'https://www.linkedin.com/in/jason-kim-0105/',
+    },
+    {
+      dev: 'Jon Zamora',
+      github: 'https://github.com/jayzizzle',
+      linkedIn: 'https://www.linkedin.com/in/jon-zamora-97a8a6219/',
+    },
+  ];
 
-    <div className="team-page">
+  return (
+    <div className='team-page'>
       <div className='d-flex justify-content-center space-above'>
         <Link to='/dashboard'>
           <h1 className='title'>
-            PlantPress<span className='leaf-icon'><ImLeaf/></span>&nbsp;&nbsp;
+            PlantPress
+            <span className='leaf-icon'>
+              <ImLeaf />
+            </span>
+            &nbsp;&nbsp;
           </h1>
-        </Link>&nbsp;&nbsp;
-        <br/>
-        <br/>
-        <br/>
+        </Link>
+        &nbsp;&nbsp;
       </div>
 
       <div className='d-flex justify-content-center'>
-        <h1>Meet the Plant Press team!</h1>
+        <h2 className='subtitle'>About Us</h2>
       </div>
 
-      <div className='d-flex justify-content-center'>
-
-        <div className="team-member">
-          <ul>
-            <li className="member-name">
-              Danny Feng
-            </li>
-            <li>
-              <a target="_blank" rel="noreferrer" href="https://github.com/dannyfeng1">
-                <ion-icon name="logo-github"></ion-icon>   Github
+      <div className='container-fluid space-above'>
+        {team.map((member, i) => (
+          <div key={i}>
+            <div className='d-flex justify-content-center'>
+              <h3 className='purple margin-top'>{member.dev}</h3>
+            </div>
+            <div className='d-flex justify-content-center'>
+              <a href={member.github} target='_blank'>
+                <FaGithub />
               </a>
-            </li>
-            <li>
-              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/danny-feng1/">
-                <ion-icon name="logo-linkedin"></ion-icon>   LinkedIn
+              &nbsp;
+              <a href={member.linkedIn} target='_blank'>
+                <FaLinkedin />
               </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="team-member">
-          <ul>
-            <li className="member-name">
-              Kent Zhang
-            </li>
-            <li>
-              <a target="_blank" rel="noreferrer" href="https://github.com/kentzhang06">
-                <ion-icon name="logo-github"></ion-icon>   Github
-              </a>
-            </li>
-            <li>
-              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/kent-zhang-297591193/">
-                <ion-icon name="logo-linkedin"></ion-icon>   LinkedIn
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="team-member">
-          <ul>
-            <li className="member-name">
-              Jon Zamora
-            </li>
-            <li>
-              <a target="_blank" rel="noreferrer" href="https://github.com/jayzizzle">
-                <ion-icon name="logo-github"></ion-icon>   Github
-              </a>
-            </li>
-            <li>
-              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/jon-zamora-97a8a6219/">
-                <ion-icon name="logo-linkedin"></ion-icon>   LinkedIn
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="team-member">
-          <ul>
-            <li className="member-name">
-              Jason Kim
-            </li>
-            <li>
-              <a target="_blank" rel="noreferrer" href="https://github.com/jasonkim0105">
-                <ion-icon name="logo-github"></ion-icon>   Github
-              </a>
-            </li>
-            <li>
-              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/jason-kim-0105/">
-                <ion-icon name="logo-linkedin"></ion-icon>   LinkedIn
-              </a>
-            </li>
-          </ul>
-        </div>
-
+            </div>
+          </div>
+        ))}
       </div>
+      <div className='row-end'></div>
     </div>
-  )
-}
+  );
+};
 
 export default TeamPage;
