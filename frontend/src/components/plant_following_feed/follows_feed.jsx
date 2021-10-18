48e4c79efe376a213cf660ea82649f6ef3fda0e3
jayzizzle <dj.jonzamora@gmail.com>
2021-10-17 18:26:22 -0700

    add heading to feed

--- frontend/src/components/plant_following_feed/follows_feed.jsx
+++ frontend/src/components/plant_following_feed/follows_feed.jsx
@@ -1,20 +1,19 @@
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 class FollowsFeed extends React.Component {
   componentDidMount() {
-    this.props.fetchFollowingPosts()
-      .then(() => this.props.fetchFollows());
+    this.props.fetchFollowingPosts().then(() => this.props.fetchFollows());
   }
 
   followPlantButton(e, plantId) {
     e.preventDefault();
-    this.props.followPlant(plantId)
+    this.props.followPlant(plantId);
   }
 
   unfollowPlantButton(e, plantId) {
     e.preventDefault();
-    this.props.unfollowPlant(plantId)
+    this.props.unfollowPlant(plantId);
   }
 
   render() {
@@ -24,23 +23,29 @@ class FollowsFeed extends React.Component {
     const followUnfollowButton = (plantId) => {
       if (follows.includes(plantId)) {
         return (
-          <button className='follow-btn' onClick={(e) => this.unfollowPlantButton(e, plantId)}>
+          <button
+            className='follow-btn'
+            onClick={(e) => this.unfollowPlantButton(e, plantId)}
+          >
             Following
           </button>
-        )
-      }else {
+        );
+      } else {
         return (
-          <button className='follow-btn' onClick={(e) => this.followPlantButton(e, plantId)}>
+          <button
+            className='follow-btn'
+            onClick={(e) => this.followPlantButton(e, plantId)}
+          >
             Follow
           </button>
-        )
+        );
       }
-    }
+    };
 
     const displayPosts = posts.map((post, i) => {
       let newDate = new Date(post.createdAt);
       const date = newDate.toDateString();
-      
+
       if (i > 10) return null;
       return (
         <div key={i}>
@@ -50,7 +55,7 @@ class FollowsFeed extends React.Component {
           </div>
           <Link to={`/plant/${post.plantId}`}>
             <div className='img-container'>
-              <img className='feed-img' src={post.imageUrl} alt=""/>
+              <img className='feed-img' src={post.imageUrl} alt='' />
             </div>
           </Link>
           <div className='feed-caption'>
@@ -58,16 +63,18 @@ class FollowsFeed extends React.Component {
             <p className='feed-text'>{post.caption}</p>
           </div>
         </div>
-      )
-      
+      );
     });
 
-    return(
+    return (
       <div className='container-fluid clear-margin'>
-        { displayPosts }
+        <div className='d-flex justify-content-center bg-green'>
+          <h2 className='subtitle heading-height'>Following Feed</h2>
+        </div>
+        {displayPosts}
         <div className='row-end'></div>
       </div>
-    )
+    );
   }
 }
 
