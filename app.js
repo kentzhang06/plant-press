cb69378db4af8e2bc90de149d40b3fe923aa2b7f
jayzizzle <dj.jonzamora@gmail.com>
2021-10-20 07:53:57 -0700

    add favicon middleware

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
-const express = require("express");
+const express = require('express');
+const favicon = require('serve-favicon');
 const app = express();
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const path = require('path');
 
 //require routes
-const users = require("./routes/api/users");
-const plants = require("./routes/api/plants");
-const posts = require("./routes/api/posts")
-const uploads = require("./routes/api/uploads");
-const reminders = require("./routes/api/reminders")
+const users = require('./routes/api/users');
+const plants = require('./routes/api/plants');
+const posts = require('./routes/api/posts');
+const uploads = require('./routes/api/uploads');
+const reminders = require('./routes/api/reminders');
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
+  });
 }
 
 app.use(passport.initialize());
@@ -25,19 +27,18 @@ require('./config/passport')(passport);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 // Routes
-app.use("/api/users", users);
-app.use("/api/plants", plants);
-app.use("/api/uploads", uploads);
-app.use("/api/posts", posts);
-app.use("/api/reminders", reminders);
+app.use('/api/users', users);
+app.use('/api/plants', plants);
+app.use('/api/uploads', uploads);
+app.use('/api/posts', posts);
+app.use('/api/reminders', reminders);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
